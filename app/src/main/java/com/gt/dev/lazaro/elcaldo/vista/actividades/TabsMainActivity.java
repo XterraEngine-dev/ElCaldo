b706c53d3dbc6c145f944ddd951c26b909b62250
Lazarus <flazaro@veentrix.com>
2016-07-28 05:07:36 -0600

    Edición de elementos del NavDrawer.

--- app/src/main/java/com/gt/dev/lazaro/elcaldo/vista/actividades/TabsMainActivity.java
+++ app/src/main/java/com/gt/dev/lazaro/elcaldo/vista/actividades/TabsMainActivity.java
@@ -1,10 +1,11 @@
 package com.gt.dev.lazaro.elcaldo.vista.actividades;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,8 +13,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.gt.dev.lazaro.elcaldo.R;
+import com.gt.dev.lazaro.elcaldo.controlador.Preferencias;
+import com.gt.dev.lazaro.elcaldo.vista.coffe.CafeMainActivity;
 
 public class TabsMainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -82,17 +86,43 @@ public class TabsMainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_otras_comidas) {
+            startActivity(new Intent(TabsMainActivity.this, OtrasComidasActivity.class));
+        } else if (id == R.id.nav_bebidas_tipicas) {
+            startActivity(new Intent(TabsMainActivity.this, BebidasActivity.class));
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_preferencias) {
+            startActivity(new Intent(TabsMainActivity.this, Preferencias.class));
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_comousar) {
+            startActivity(new Intent(TabsMainActivity.this, ComoUsarActivity.class));
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_website) {
+            String urlWeb = "http://elcaldo.net84.net";
+            Intent iWeb = new Intent(Intent.ACTION_VIEW);
+            iWeb.setData(Uri.parse(urlWeb));
+            startActivity(iWeb);
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_twitter) {
+            String urlTweet = "http://twitter.com/elcaldoapp";
+            Intent iTweet = new Intent(Intent.ACTION_VIEW);
+            iTweet.setData(Uri.parse(urlTweet));
+            startActivity(iTweet);
+
+        } else if (id == R.id.nav_facebook) {
+            String urlFB = "http://facebook.com/elcaldoapp";
+            Intent iFB = new Intent(Intent.ACTION_VIEW);
+            iFB.setData(Uri.parse(urlFB));
+            startActivity(iFB);
+
+        } else if (id == R.id.nav_terminos) {
+            startActivity(new Intent(TabsMainActivity.this, CopyrightActivity.class));
+
+        } else if (id == R.id.nav_cafe) {
+            startActivity(new Intent(TabsMainActivity.this, CafeMainActivity.class));
+
+        } else if (id == R.id.nav_creditos) {
+            startActivity(new Intent(TabsMainActivity.this, CreditosActivity.class));
 
         }
 
